44bd8d94005c50c86cad915956eb61987392df5b
Orange-Ice28 <2253547078@qq.com>
2026-07-12 00:12:19 +0800

    修正默认首页空白问题

--- bupt-ranking/vite.config.ts
+++ bupt-ranking/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/BUPT_ICPC_rankings/',
   plugins: [
     vue(),
     vueDevTools(),
