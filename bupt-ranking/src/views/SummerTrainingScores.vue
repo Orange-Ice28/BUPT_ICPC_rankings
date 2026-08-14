b3bcbaf26fea5fec7cf6493d1ac12264956d2282
Orange-Ice28 <2253547078@qq.com>
2026-08-14 17:13:44 +0800

    更新暑期集训牛客第9场成绩

--- bupt-ranking/src/views/SummerTrainingScores.vue
+++ bupt-ranking/src/views/SummerTrainingScores.vue
@@ -270,7 +270,7 @@ function formatContestValue(value: string | number, isScore: boolean = false): s
                 <td>7</td>
                 <td>8</td>
                 <td>9</td>
-                <td>-</td>
+                <td>9</td>
                 <td>-</td>
                 <td>7</td>
                 <td>8</td>
